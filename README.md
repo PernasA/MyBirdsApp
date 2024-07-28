850aa807d0dfcdede71296bc064aa99fa0bb9a98
PernasA <agustinbaltazarpernas@gmail.com>
2024-07-28 20:48:14 -0300

    Create APK and reduce image sizes
    - Built and added the APK for easier testing. Actual size: 22,6 MB
    - Compressed and optimized multiple images to reduce file size

---
+++ README.md
@@ -0,0 +1,59 @@
+# Aves de Merlo San Luis
+
+Aves de Merlo San Luis es una aplicación móvil diseñada para ayudarte a identificar y registrar las aves que observas en la región de Merlo, San Luis. La aplicación permite a los usuarios almacenar una lista de aves observadas y proporciona una interfaz fácil de usar para la navegación y el registro de observaciones.
+
+## Comenzando
+
+Estas instrucciones te guiarán en la configuración del entorno necesario para ejecutar la aplicación localmente en tu máquina o cómo descargarla desde Google Play Store.
+
+### Prerrequisitos
+
+Para probar la aplicación localmente, se necesita:
+- Android Studio instalado en tu computadora.
+- Un emulador de Android configurado en Android Studio.
+
+### Instalación
+
+1. Clona el repositorio del proyecto en tu máquina local.
+git clone https://github.com/tu_usuario/aves-de-merlo-san-luis.git
+Abre Android Studio y selecciona "Open an existing Android Studio project". Navega hasta el directorio donde clonaste el repositorio y selecciónalo. Asegúrate de que el emulador de Android esté configurado y funcionando.
+Haz clic en el botón "Run" (el icono de play) en Android Studio para compilar y ejecutar la aplicación en el emulador.
+
+2. Descarga desde Google Play Store (En proceso de verificación)
+Puedes descargar e instalar la aplicación directamente desde la Google Play Store. 
+
+## Uso
+
+La aplicación permite a los usuarios:
+
+1. Navegar a través de diferentes secciones utilizando NavHost.
+2. Registrar aves observadas y almacenarlas localmente utilizando Room.
+3. Consultar una lista de aves observadas.
+4. Observar los distintos recorridos de observación en los alrededores de Merlo San Luis.
+
+## Arquitectura y Tecnologías Utilizadas
+
+1. MVVM: Arquitectura utilizada para separar las preocupaciones y facilitar el mantenimiento.
+2. Jetpack Compose: Utilizado para construir la UI de forma declarativa.
+3. Coroutines: Para la gestión de tareas asincrónicas.
+4. Room: Para el almacenamiento local de datos.
+5. Patrón Singleton: Para gestionar el contador de aves observadas.
+6. Archivos JSON: Para la gestión y carga de datos.
+7. NavHost: Para la navegación dentro de la aplicación.
+8. Mockk para los tests unitarios y de integración.
+9. AndroidJUnit4 para los tests instrumentados.
+10. Git y Github para el control de versiones.
+
+# CRÉDITOS
+
+## Autor
+
+    Ing. Agustin Pernas - Desarrollador de la aplicación - [Perfil de LinkedIn](https://www.linkedin.com/in/agustinbpernas/)
+
+## Agradecimientos
+
+    Raúl Balla, escritor del libro que me inspiró a desarrollar esta aplicación.
+
+## Licencia
+
+Este proyecto está licenciado bajo la Licencia MIT - mira el archivo [LICENSE.md](LICENSE.md) para más detalles.
--- app/src/main/java/com/example/mybirdsapp/utils/DrawableResourcesList.kt
+++ app/src/main/java/com/example/mybirdsapp/utils/DrawableResourcesList.kt
@@ -4,50 +4,50 @@ import com.example.mybirdsapp.R
 
 object DrawableResourcesList {
     val drawableListBirds = listOf(
-        R.drawable.icon_orange_bird_draw, R.drawable.icon_orange_bird_draw, // 1 2
-        R.drawable.icon_orange_bird_draw, R.drawable.icon_orange_bird_draw, R.drawable.icon_orange_bird_draw, // 3 4 5
-        R.drawable.icon_orange_bird_draw, R.drawable.icon_orange_bird_draw, R.drawable.icon_orange_bird_draw, // 6 7 8
-        R.drawable.icon_orange_bird_draw, R.drawable.icon_orange_bird_draw, R.drawable.icon_orange_bird_draw, // 9 10 11
-        R.drawable.b12_condor_andino, R.drawable.icon_orange_bird_draw, R.drawable.icon_orange_bird_draw, // 12 13 14
-        R.drawable.icon_orange_bird_draw, R.drawable.b16_coscoroba, R.drawable.icon_orange_bird_draw, // 15 16 17
+        R.drawable.icon_bird, R.drawable.icon_bird, // 1 2
+        R.drawable.icon_bird, R.drawable.icon_bird, R.drawable.icon_bird, // 3 4 5
+        R.drawable.icon_bird, R.drawable.icon_bird, R.drawable.icon_bird, // 6 7 8
+        R.drawable.icon_bird, R.drawable.icon_bird, R.drawable.icon_bird, // 9 10 11
+        R.drawable.b12_condor_andino, R.drawable.icon_bird, R.drawable.icon_bird, // 12 13 14
+        R.drawable.icon_bird, R.drawable.b16_coscoroba, R.drawable.icon_bird, // 15 16 17
         R.drawable.b18_pato_barcino, R.drawable.b19_pato_zambullidor_chico, R.drawable.b20_aguila_mora, // 18 19 20
-        R.drawable.icon_orange_bird_draw, R.drawable.icon_orange_bird_draw, R.drawable.icon_orange_bird_draw, // 21 22 23
-        R.drawable.icon_orange_bird_draw, R.drawable.icon_orange_bird_draw, R.drawable.icon_orange_bird_draw, // 24 25 26
-        R.drawable.icon_orange_bird_draw, R.drawable.icon_orange_bird_draw, R.drawable.b29_tero_real, // 27 28 29
-        R.drawable.b30_pitotoy_grande, R.drawable.icon_orange_bird_draw, R.drawable.icon_orange_bird_draw, // 30 31 32
-        R.drawable.icon_orange_bird_draw, R.drawable.b34_catita_serrana_grande, R.drawable.icon_orange_bird_draw, // 33 34 35
-        R.drawable.b36_pirincho, R.drawable.b37_crespin, R.drawable.icon_orange_bird_draw, // 36 37 38
-        R.drawable.b39_cabure, R.drawable.b40_lechucita_vizcachera, R.drawable.icon_orange_bird_draw, // 39 40 41
-        R.drawable.b42_loro_barranquero, R.drawable.b43_picaflor_cometa, R.drawable.b44_martin_pescador_chico, // 42 43 44
-        R.drawable.b45_durmili, R.drawable.b46_carpintero_real, R.drawable.icon_orange_bird_draw, // 45 46 47
-        R.drawable.b48_carpintero_lomo_blanco, R.drawable.icon_orange_bird_draw, R.drawable.icon_orange_bird_draw, // 48 49 50
+        R.drawable.icon_bird, R.drawable.icon_bird, R.drawable.icon_bird, // 21 22 23
+        R.drawable.icon_bird, R.drawable.icon_bird, R.drawable.icon_bird, // 24 25 26
+        R.drawable.icon_bird, R.drawable.icon_bird, R.drawable.b29_tero_real, // 27 28 29
+        R.drawable.b30_pitotoy_grande, R.drawable.icon_bird, R.drawable.icon_bird, // 30 31 32
+        R.drawable.b33_loro_barranquero, R.drawable.b34_catita_serrana_grande, R.drawable.icon_bird, // 33 34 35
+        R.drawable.b36_pirincho, R.drawable.b37_crespin, R.drawable.icon_bird, // 36 37 38
+        R.drawable.b39_cabure, R.drawable.b40_lechucita_vizcachera, R.drawable.icon_bird, // 39 40 41
+        R.drawable.icon_bird, R.drawable.b43_picaflor_cometa, R.drawable.b44_martin_pescador_chico, // 42 43 44
+        R.drawable.b45_durmili, R.drawable.b46_carpintero_real, R.drawable.icon_bird, // 45 46 47
+        R.drawable.b48_carpintero_lomo_blanco, R.drawable.icon_bird, R.drawable.icon_bird, // 48 49 50
         R.drawable.b51_remolinera_serrana, R.drawable.b52_remolinera_chocolate, R.drawable.b53_hornero, // 51 52 53
-        R.drawable.icon_orange_bird_draw, R.drawable.b55_crestudo, R.drawable.b56_curutie_blanco, // 54 55 56
-        R.drawable.b57_canastero_palido, R.drawable.b58_espartillero_serrano, R.drawable.icon_orange_bird_draw, // 57 58 59
+        R.drawable.icon_bird, R.drawable.b55_crestudo, R.drawable.b56_curutie_blanco, // 54 55 56
+        R.drawable.b57_canastero_palido, R.drawable.b58_espartillero_serrano, R.drawable.icon_bird, // 57 58 59
         R.drawable.b60_coludito_copeton, R.drawable.b61_cacholote_castanio, R.drawable.b62_chinchero_grande, // 60 61 62
-        R.drawable.icon_orange_bird_draw, R.drawable.icon_orange_bird_draw, R.drawable.b65_gallito_de_collar, // 63 64 65
-        R.drawable.icon_orange_bird_draw, R.drawable.icon_orange_bird_draw, R.drawable.b68_churrinche, // 66 67 68
-        R.drawable.b69_cachudito_pico_negro, R.drawable.icon_orange_bird_draw, R.drawable.b71_benteveo, // 69 70 71
-        R.drawable.b72_picabuey, R.drawable.b73_birro, R.drawable.icon_orange_bird_draw, // 72 73 74
-        R.drawable.icon_orange_bird_draw, R.drawable.icon_orange_bird_draw, R.drawable.icon_orange_bird_draw, // 75 76 77
-        R.drawable.icon_orange_bird_draw, R.drawable.icon_orange_bird_draw, R.drawable.icon_orange_bird_draw, // 78 79 80
-        R.drawable.icon_orange_bird_draw, R.drawable.icon_orange_bird_draw, R.drawable.icon_orange_bird_draw, // 81 82 83
-        R.drawable.icon_orange_bird_draw, R.drawable.icon_orange_bird_draw, R.drawable.icon_orange_bird_draw, // 84 85 86
-        R.drawable.icon_orange_bird_draw, R.drawable.b88_zorzal_chiguanco, R.drawable.icon_orange_bird_draw, // 87 88 89
-        R.drawable.icon_orange_bird_draw, R.drawable.icon_orange_bird_draw, R.drawable.icon_orange_bird_draw, // 90 91 92
-        R.drawable.icon_orange_bird_draw, R.drawable.icon_orange_bird_draw, R.drawable.icon_orange_bird_draw, // 93 94 95
-        R.drawable.icon_orange_bird_draw, R.drawable.icon_orange_bird_draw, R.drawable.icon_orange_bird_draw, // 96 97 98
-        R.drawable.icon_orange_bird_draw, R.drawable.icon_orange_bird_draw, R.drawable.icon_orange_bird_draw, // 99 100 101
-        R.drawable.icon_orange_bird_draw, R.drawable.icon_orange_bird_draw, R.drawable.icon_orange_bird_draw, // 102 103 104
-        R.drawable.icon_orange_bird_draw, R.drawable.icon_orange_bird_draw, R.drawable.icon_orange_bird_draw, // 105 106 107
-        R.drawable.icon_orange_bird_draw, R.drawable.icon_orange_bird_draw, R.drawable.icon_orange_bird_draw, // 108 109 110
-        R.drawable.icon_orange_bird_draw, R.drawable.icon_orange_bird_draw, R.drawable.icon_orange_bird_draw, // 111 112 113
-        R.drawable.icon_orange_bird_draw, R.drawable.icon_orange_bird_draw, R.drawable.icon_orange_bird_draw, // 114 115 116
-        R.drawable.icon_orange_bird_draw, R.drawable.icon_orange_bird_draw, R.drawable.icon_orange_bird_draw, // 117 118 119
-        R.drawable.icon_orange_bird_draw, R.drawable.icon_orange_bird_draw, R.drawable.icon_orange_bird_draw, // 120 121 122
-        R.drawable.icon_orange_bird_draw, R.drawable.icon_orange_bird_draw, R.drawable.icon_orange_bird_draw, // 123 124 125
-        R.drawable.icon_orange_bird_draw, R.drawable.icon_orange_bird_draw, R.drawable.icon_orange_bird_draw, // 126 127 128
-        R.drawable.icon_orange_bird_draw, R.drawable.icon_orange_bird_draw, R.drawable.icon_orange_bird_draw, // 129 130 131
+        R.drawable.icon_bird, R.drawable.icon_bird, R.drawable.b65_gallito_de_collar, // 63 64 65
+        R.drawable.icon_bird, R.drawable.icon_bird, R.drawable.b68_churrinche, // 66 67 68
+        R.drawable.b69_cachudito_pico_negro, R.drawable.icon_bird, R.drawable.b71_benteveo, // 69 70 71
+        R.drawable.b72_picabuey, R.drawable.b73_birro, R.drawable.icon_bird, // 72 73 74
+        R.drawable.icon_bird, R.drawable.icon_bird, R.drawable.icon_bird, // 75 76 77
+        R.drawable.icon_bird, R.drawable.icon_bird, R.drawable.icon_bird, // 78 79 80
+        R.drawable.icon_bird, R.drawable.icon_bird, R.drawable.icon_bird, // 81 82 83
+        R.drawable.icon_bird, R.drawable.icon_bird, R.drawable.icon_bird, // 84 85 86
+        R.drawable.icon_bird, R.drawable.b88_zorzal_chiguanco, R.drawable.icon_bird, // 87 88 89
+        R.drawable.icon_bird, R.drawable.icon_bird, R.drawable.icon_bird, // 90 91 92
+        R.drawable.icon_bird, R.drawable.icon_bird, R.drawable.icon_bird, // 93 94 95
+        R.drawable.icon_bird, R.drawable.icon_bird, R.drawable.icon_bird, // 96 97 98
+        R.drawable.icon_bird, R.drawable.icon_bird, R.drawable.icon_bird, // 99 100 101
+        R.drawable.icon_bird, R.drawable.icon_bird, R.drawable.icon_bird, // 102 103 104
+        R.drawable.icon_bird, R.drawable.icon_bird, R.drawable.icon_bird, // 105 106 107
+        R.drawable.icon_bird, R.drawable.icon_bird, R.drawable.icon_bird, // 108 109 110
+        R.drawable.icon_bird, R.drawable.icon_bird, R.drawable.icon_bird, // 111 112 113
+        R.drawable.icon_bird, R.drawable.icon_bird, R.drawable.icon_bird, // 114 115 116
+        R.drawable.icon_bird, R.drawable.icon_bird, R.drawable.icon_bird, // 117 118 119
+        R.drawable.icon_bird, R.drawable.icon_bird, R.drawable.icon_bird, // 120 121 122
+        R.drawable.icon_bird, R.drawable.icon_bird, R.drawable.icon_bird, // 123 124 125
+        R.drawable.icon_bird, R.drawable.icon_bird, R.drawable.icon_bird, // 126 127 128
+        R.drawable.icon_bird, R.drawable.icon_bird, R.drawable.icon_bird, // 129 130 131
         )
 
     val drawableListObservationRoutes = listOf(
