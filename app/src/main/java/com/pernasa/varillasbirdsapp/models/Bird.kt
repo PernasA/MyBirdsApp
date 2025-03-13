368c43ea2f1b6e49f493941581034d8b848ba995
PernasA <agustinbaltazarpernas@gmail.com>
2025-03-12 21:06:13 -0300

    Commit updating the jsons

--- app/src/main/java/com/pernasa/varillasbirdsapp/models/Bird.kt
+++ app/src/main/java/com/pernasa/varillasbirdsapp/models/Bird.kt
@@ -3,11 +3,11 @@ package com.pernasa.varillasbirdsapp.models
 data class Bird (
     val id: Int = 0,
     val name: String = "",
-    val heightLocation: Int = 0,
+    val heightLocation: Int? = 0,
     val height: Int = 0,
     val frequency: Int = 0,
     val scientificName: String = "",
-    val englishName: String = "",
+    val englishName: String? = "",
     val description: String = "",
     val imageResId: Int = 0
 )
--- app/src/main/java/com/pernasa/varillasbirdsapp/models/BirdJson.kt
+++ app/src/main/java/com/pernasa/varillasbirdsapp/models/BirdJson.kt
@@ -9,9 +9,9 @@ data class BirdJson (
     val frequency: Int = 0,
     val description: String = "",
     @SerializedName("height_location")
-    val heightLocation: Int = 0,
+    val heightLocation: Int? = 0,
     @SerializedName("scientific_name")
     val scientificName: String = "",
     @SerializedName("english_name")
-    val englishName: String = ""
+    val englishName: String? = ""
 )
